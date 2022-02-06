8016655a0b5ac12fd82062b90f166452b18f17b0
Mauro Ezequiel Mamani <mauroezequielmamani@Mauros-MacBook-Pro.local>
2022-02-06 01:31:54 -0300

    Changed tab icon and text and styled Welcome screen

--- src/screens/Welcome/index.js
+++ src/screens/Welcome/index.js
@@ -6,14 +6,24 @@ import styles from './styles.module.scss'
 const Welcome = () => {
   const navigate = useNavigate()
 
+  const navigateToPartyForm = () => navigate(Routes.PARTY_FORM)
+
   return (
     <div className={styles.container}>
       <h1>Welcome to Crystl party planner, we will help you to plan the best beer parties!</h1>
-      <span>
-        Please fill in some information we are going to ask you, this will help us to get the correct amount of beers
-        you will need and generate an invitation for you
+      <h2>What can I do with this app?</h2>
+      <span className={styles.text}>
+        You will be able to calculate the ideal amount of beers for your party to be unforgivable! We will calculate this amount depending on
+        the amount of participants you invite and the temperature for the given day.
       </span>
-      <button onClick={() => navigate(Routes.PARTY_FORM)} type='button'>Go to Party Form</button>
+      <span className={styles.text}>
+        You will also be able to generate an invitation that you can send to all your invitees!
+      </span>
+      <h2>What do I have to do?</h2>
+      <span className={styles.text}>
+        You only need to fill in some information for us and we will do the rest for you!
+      </span>
+      <button onClick={navigateToPartyForm} type='button' className={styles.next}>Lets get the party planned!</button>
     </div>
   )
 }
